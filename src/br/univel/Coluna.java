a71c48f071cf7cec5bc806a463d6ac7682b6d33e
Mauricio Ouriques Toneto <matt_ouriques@hotmail.com>
2016-04-20 21:20:45 -0300

    Criada classe Cliente, annotacion tabela e coluna

---
+++ src/br/univel/Coluna.java
@@ -0,0 +1,14 @@
+package br.univel;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Coluna {
+
+	String nome() default "";
+	boolean pk() default false;
+}

302b1c11093cdc4f4a912fdec885ed16e8a6e827
Mauricio Ouriques Toneto <matt_ouriques@hotmail.com>
2016-04-26 11:08:34 -0300

    classe cliente atualizada

--- src/br/univel/Cliente.java
+++ src/br/univel/Cliente.java
@@ -10,42 +10,17 @@ public class Cliente {
 	private String endereço;
 	@Coluna(nome="CLTELEFONE")
 	private String telefone;
-	//private EstadoCivil estadoCivil = EstadoCivil.CASADO;
+	private EstadoCivil estadoCivil;
 	public Cliente() {
 		this(0, null, null, null, null);
 	}
 
-	public Cliente(int id, String nome, String endereço, String telefone, Object object) {
+	public Cliente(int id, String nome, String endereço, String telefone, EstadoCivil estadocivil) {
 		super();
 		this.id = id;
 		this.nome = nome;
-		this.nome = endereço;
-		this.nome = telefone;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getEndereço() {
-		return endereço;
-	}
-	public void setEndereço(String endereço) {
 		this.endereço = endereço;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
 		this.telefone = telefone;
-	}
-		
+		this.estadoCivil = estadocivil;
+	}	
 }
